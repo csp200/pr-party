22fa1ae4d93ecf08738209a46b36a0a66cd1c306
Tyson Haufschild <thaufschild@hawk.illinoistech.edu>
2025-10-22 15:43:12 -0500

    added function calling tyson.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import greetings
 def lee_hello():
     greetings.lee.hello()
 
+# Greeting for tyson.py
+def tyson_hello():
+    greetings.tyson.hello()
+
 # add a call to your function in main, below the heading output: 
 def main():
     print("Fall 2025 CSP 200 PR Yearbook")
