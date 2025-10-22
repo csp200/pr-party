b3afc36a0b4e1d1e8fb94302b30ca19400f4e9a5
Monica Cardenas <mcardenas6@hawk.illinoistech.edu>
2025-10-22 15:59:57 -0500

    New function

--- greetings/mcardenas.py
+++ greetings/mcardenas.py
@@ -1,2 +1,8 @@
 def mcardenas_hello():
     greetings.mcardenas.hello()
+def main():
+    print("Fall 2025 CSP 200 PR Yearbook")
+    print("=============================")
+
+    mcardenas_hello()
+    
