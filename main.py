a5d2f93806a1fa081911236afc869dc2c56eb764
Ben Baron Birnbaum <bbirnbaum@hawk.illinoistech.edu>
2025-10-22 15:53:21 -0500

    gretting thing but it works

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import greetings
 
 # define a function that calls your library definition below:
 def lee_hello():
-    greetings.lee.hello()
+    greetings.lee.hello();
 
+def ben_hello():
+    greetings.ben.hello();
 # add a call to your function in main, below the heading output: 
 def main():
     print("Fall 2025 CSP 200 PR Yearbook")
-    print("Help Me I am trapped in this spaghetti code")
+    print("=============================")
     
     lee_hello()
+    ben_hello()
 
 # don't touch this!
 if __name__ == "__main__":
